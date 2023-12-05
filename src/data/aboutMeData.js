a4c8cf67ff8efe98244b86c559f833dc4414777b
Naufal Ardhani <naufalardhani89@gmail.com>
2023-12-05 13:43:04 +0700

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,11 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 
+		"Saya adalah mahasiswa yang tengah menempuh studi di IPB University dengan minat utama dalam keamanan dunia maya atau yang lebih dikenal sebagai cybersecurity. Aktif terlibat dalam kompetisi Capture The Flag (CTF) di tingkat nasional dan internasional, saya bangga memimpin dua tim yang berfokus pada keamanan siber, yaitu Cyber Security IPB dan Social Engineering Experts Singapore. Pencapaian signifikan dalam beberapa lomba CTF, baik di tingkat nasional maupun internasional, telah memberikan wawasan mendalam tentang cara mengatasi tantangan keamanan siber yang semakin kompleks. Keberhasilan ini juga memperkuat komitmen saya untuk terus mengembangkan keahlian dan kontribusi dalam menjaga integritas keamanan informasi.",
+	},
+	{
+		id: 2,
+		bio: "Di samping perjalanan sukses dalam kompetisi, saya menjalani perjalanan pendidikan di IPB University untuk memperdalam pemahaman saya di bidang teknologi informasi dan keamanan siber. Kombinasi antara pengalaman lapangan dan pembelajaran akademis memberi saya landasan kuat untuk menghadapi tantangan keamanan siber modern. Dengan memimpin tim dan terus belajar, saya bertekad membawa kontribusi positif dalam upaya melindungi dan mengamankan dunia maya dari ancaman yang terus berkembang.",
+	},
+];
